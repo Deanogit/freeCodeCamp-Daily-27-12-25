be8510f082fe2f6055bd9d27988a8bdea82857ca
Dean Howe <hideanhowe@gmail.com>
2026-01-12 22:09:30 +0000

    solution

--- index.js
+++ index.js
@@ -10,3 +10,25 @@
 // "Player 1 wins" if Player 1 wins.
 // "Player 2 wins" if Player 2 wins.
 // "Tie" if both players choose the same option.
+
+function rockPaperScissors(player1, player2) {
+  console.log(player1, player2);
+
+  // tie
+  if (player1 === player2) {
+    return 'Tie';
+  }
+
+  // Rock beats Scissors
+  if (
+    (player1 === 'Rock' && player2 === 'Scissors') ||
+    (player1 === 'Paper' && player2 === 'Rock') ||
+    (player1 === 'Scissors' && player2 === 'Paper')
+  ) {
+    return 'Player 1 wins';
+  } else {
+    return 'Player 2 wins';
+  }
+
+  return player1;
+}
